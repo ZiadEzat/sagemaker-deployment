03dc8d7d53a82fd2601589dd8f6ea31d8fb3c31d
EC2 Default User <ec2-user@ip-172-16-85-89.us-east-2.compute.internal>
2020-02-15 18:37:56 +0000

    deployed model

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -72,8 +72,7 @@ def predict_fn(input_data, model):
     #         data_len - The length of the review
 
    
-    data_X,data_len = convert_and_pad(model.word_dict,review_to_words(input_data))
-
+    data_X, data_len = convert_and_pad(model.word_dict, review_to_words(input_data))
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
     data_pack = np.hstack((data_len, data_X))
@@ -88,7 +87,7 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
     with torch.no_grad():
-        output = model(data)
+        output = model.forward(data)
     result = np.round(output.numpy())
 
-    
+    return result
